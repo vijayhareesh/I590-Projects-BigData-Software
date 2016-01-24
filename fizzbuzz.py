85af7016a1b0f95cfe307ac9f4437be118edd11a
vijayhareesh <munnavjy@gmail.com>
2016-01-23 22:09:06 -0500

    edit acc to prob desc

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,25 @@
-from sys import argv
 #please input the parameter while running the python file
 #Execution: python fizzbuzz.py <number>
 #Example execution: python fizzbuzz.py 15
 
-def fizzbuzz(newnum):
-	if newnum%2==0 and newnum%3==0:
-		print "fizzbuzz"
-	elif newnum%2==0:
-		print "fizz"
-	elif newnum%3==0:
-		print "buzz"
-	else:
-		print newnum
+from sys import argv
+def fizzbuzz(num):
+	newnum = 1
+	while newnum<=num:
+		if newnum%2==0 and newnum%3==0:
+			print "fizzbuzz"
+		elif newnum%2==0:
+			print "fizz"
+		elif newnum%3==0:
+			print "buzz"
+		else:
+			print newnum
+		newnum = newnum+1
 
 def __main__():
 	num = int(argv[1])
-	i=1
-	while i<=num:
-		fizzbuzz(i)
-		i=i+1
+	fizzbuzz(num)
+	#while i<=num:
+	#	fizzbuzz(i)
+	#	i=i+1
 __main__()
